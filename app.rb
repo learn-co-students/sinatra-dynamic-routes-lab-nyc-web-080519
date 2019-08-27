bc53b6e409b34417f5764a9189dc28e2bbf1877a
github username <github email address>
2019-08-26 20:36:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,49 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    @reversed_name = params[:name].reverse
+    "#{@reversed_name}"
+  end
+
+  get '/square/:number' do
+    @squared_number = (params[:number].to_i ** 2).to_s
+    "#{@squared_number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @phrase = params[:phrase] * (params[:number].to_i)
+    "#{@phrase}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation] == "add"
+      @sum = params[:number1].to_i + params[:number2].to_i
+      "#{@sum}"
+    elsif params[:operation] == "subtract"
+      @difference = params[:number1].to_i - params[:number2].to_i
+      "#{@difference}"
+    elsif params[:operation] == "multiply"
+      @product = params[:number1].to_i * params[:number2].to_i
+      "#{@product}"
+    else params[:operation] == "divide"
+      @quotient = params[:number1].to_i/params[:number2].to_i
+      "#{@quotient}"
+    end
+  end
+
+
+
+
+
+
+
+
+
 
 end
